a8e37583bb33ab4ccffa863f8f7e47d8e4141f0c
marwansaputro <marwansaputro26@gmail.com>
2024-01-22 19:18:29 +0700

    Change Name

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Integra Mobile',
+      title: 'Integra Mobile New',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
         // useMaterial3: true,
