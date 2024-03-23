6cc5600a9fef94254f0577b6e08ea22dd0eaf0db
marwansaputro <marwansaputro26@gmail.com>
2024-03-23 15:31:19 +0700

    update screen product & portfolio

--- lib/screens/portfolio/section_portfolio/section_portfolio.dart
+++ lib/screens/portfolio/section_portfolio/section_portfolio.dart
@@ -49,7 +49,7 @@ class SectionPortfolio extends StatelessWidget {
               gap: 5,
               children: const [
                 CustomItemPortfolio(
-                  image: pathImageNews1,
+                  image: pathImagePortfolioUII,
                   title1: 'E-Office Universitas Islam Indonesia',
                   title2:
                       '“Aplikasi sistem persuratan elektronik berbasis web & android yang diimplementasikan di Universitas Islam Indonesia”',
@@ -57,7 +57,7 @@ class SectionPortfolio extends StatelessWidget {
                   title4: 'Pendidikan',
                 ),
                 CustomItemPortfolio(
-                  image: pathImageNews2,
+                  image: pathImagePortfolioBI,
                   title1: 'Aplikasi Big Data Inflasi Bank Indonesia DIY',
                   title2:
                       '“Aplikasi pengelolaan data & monitoring inflasi di Daerah Istimewa Yogyakarta yang dikembangkan Bank Indonesia”',
@@ -65,7 +65,7 @@ class SectionPortfolio extends StatelessWidget {
                   title4: 'Bisnis',
                 ),
                 CustomItemPortfolio(
-                  image: pathImageNews3,
+                  image: pathImagePortfolioTrenggalek,
                   title1: 'Sistem Penanggulangan Kemiskinan Kab. Trenggalek',
                   title2:
                       '“Aplikasi Pengelolaan Data dan Program Pengentasan Kemiskinan yang diimplementasikan di Kabupaten Trenggalek”',
@@ -73,7 +73,7 @@ class SectionPortfolio extends StatelessWidget {
                   title4: 'Pemerintahan',
                 ),
                 CustomItemPortfolio(
-                  image: pathImageNews4,
+                  image: pathImagePortfolioDisnaker,
                   title1: 'Sistem Aduan Tenaga Kerja Disnakertrans DIY',
                   title2:
                       '“Aplikasi Pengelolaan Aduan Tenaga Kerja yang diimplementasikan di Daerah Istimewa Yogyakarta”',
@@ -81,35 +81,68 @@ class SectionPortfolio extends StatelessWidget {
                   title4: 'Pemerintahan',
                 ),
                 CustomItemPortfolio(
-                  image: pathImageNews1,
-                  title1: 'E-Office Universitas Islam Indonesia',
+                  image: pathImagePortfolioPrawirotaman,
+                  title1: 'Sistem Reservasi Sewa Ruangan UPT Pusat Bisnis',
                   title2:
-                      '“Aplikasi sistem persuratan elektronik berbasis web & android yang diimplementasikan di Universitas Islam Indonesia”',
-                  title3: 'Universitas Islam Indonesia,',
-                  title4: 'Pendidikan',
-                ),
-                CustomItemPortfolio(
-                  image: pathImageNews2,
-                  title1: 'Aplikasi Big Data Inflasi Bank Indonesia DIY',
-                  title2:
-                      '“Aplikasi pengelolaan data & monitoring inflasi di Daerah Istimewa Yogyakarta yang dikembangkan Bank Indonesia”',
-                  title3: 'KPW Bank Indonesia DIY,',
+                      '“Aplikasi Manajemen Reservasi sewa ruangan UPT Pusat Bisnis yang diimplementasikan di Pasar Prawirotaman Yogyakarta”',
+                  title3: 'Bank BPD DIY,',
                   title4: 'Bisnis',
                 ),
                 CustomItemPortfolio(
-                  image: pathImageNews3,
-                  title1: 'Sistem Penanggulangan Kemiskinan Kab. Trenggalek',
+                  image: pathImagePortfolioSimtaru,
+                  title1: 'Sistem Informasi Tata Ruang Kabupaten Sleman',
                   title2:
-                      '“Aplikasi Pengelolaan Data dan Program Pengentasan Kemiskinan yang diimplementasikan di Kabupaten Trenggalek”',
-                  title3: 'Pemkab Trenggalek,',
+                      '“Sistem Informasi manajemen yang digunakan untuk pendataan rencana detail tata ruang di Kabupaten Sleman”',
+                  title3: 'Pemkab Sleman,',
                   title4: 'Pemerintahan',
                 ),
                 CustomItemPortfolio(
-                  image: pathImageNews4,
-                  title1: 'Sistem Aduan Tenaga Kerja Disnakertrans DIY',
+                  image: pathImagePortfolioDestinasi,
+                  title1: 'Aplikasi Tiketing dan Manajemen Destinasi Wisata',
                   title2:
-                      '“Aplikasi Pengelolaan Aduan Tenaga Kerja yang diimplementasikan di Daerah Istimewa Yogyakarta”',
+                      '“Aplikasi pengelolaan tiketing destinasi wisata dan sistem manajemen pengelolaan objek wisata”',
+                  title3: 'Destinasi Wisata Indonesia,',
+                  title4: 'Bisnis',
+                ),
+                CustomItemPortfolio(
+                  image: pathImagePortfolioAlkes,
+                  title1: 'Sistem Pengawasan Alat Kesehatan dan PKRT',
+                  title2:
+                      '“Aplikasi E-Watch & E-Pengawasan – Sistem Pengawasan Alat Kesehatan dan PKRT Kementerian Kesehatan Republik Indonesia”',
+                  title3: 'Kementerian Kesehatan,',
+                  title4: 'Pemerintahan',
+                ),
+                CustomItemPortfolio(
+                  image: pathImagePortfolioVisiting,
+                  title1: 'Aplikasi Pendataan Wisatawan Visiting Jogja',
+                  title2:
+                      '“Aplikasi Pengelolaan Data Kunjungan Wisatawan dan Reservasi Tiket Destinasi Wisata di DIY”',
                   title3: 'Pemda DIY,',
+                  title4: 'Pemerintahan',
+                ),
+                CustomItemPortfolio(
+                  image: pathImagePortfolioSPPIRT,
+                  title1:
+                      'Aplikasi Digitalisasi Layanan SP-PIRT Kota Yogyakarta',
+                  title2:
+                      '““Aplikasi Digitalisasi Layanan Sertifikasi Produk Pangan Industri Rumah Tangga Kota Yogyakarta”',
+                  title3: 'Pemkot Yogyakarta,',
+                  title4: 'Pemerintahan',
+                ),
+                CustomItemPortfolio(
+                  image: pathImagePortfolioCovid,
+                  title1: 'Aplikasi Covid Tracer Kabupaten Sleman',
+                  title2:
+                      '“Aplikasi Pengelolaan Data Covid19 dan Tracing Sistem di Kabupaten Sleman”',
+                  title3: 'Pemkab Sleman,',
+                  title4: 'Pemerintahan',
+                ),
+                CustomItemPortfolio(
+                  image: pathImagePortfolioCovid,
+                  title1: 'Aplikasi E-Health Kabupaten Blitar',
+                  title2:
+                      '“Aplikasi informasi dan Layanan publik kesehatan di Kabupaten Blitar berbasis web & mobile”',
+                  title3: 'Pemkab Blitar,',
                   title4: 'Pemerintahan',
                 ),
               ],
