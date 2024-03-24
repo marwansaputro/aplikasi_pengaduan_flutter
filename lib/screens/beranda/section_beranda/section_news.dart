c6da1fcdc407c559d6a1f6defe32015db954884a
marwansaputro <marwansaputro26@gmail.com>
2024-03-24 15:02:34 +0700

    update screen beranda news

--- lib/screens/beranda/section_beranda/section_news.dart
+++ lib/screens/beranda/section_beranda/section_news.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:integra_mobile/layout/column.dart';
 import 'package:integra_mobile/layout/row.dart';
 import 'package:integra_mobile/value/path_image.dart';
 import 'package:integra_mobile/value/theme.dart';
@@ -10,21 +11,14 @@ class SectionNews extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return IColumn(
+      gap: 10,
       children: [
         Container(
           width: 400,
           height: 250,
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(8),
-
-            // boxShadow: [
-            //   BoxShadow(
-            //       color: Theme.of(context).colorScheme.shadow.withOpacity(0.10),
-            //       blurRadius: 15,
-            //       spreadRadius: -15,
-            //       offset: const Offset(0, 15)),
-            // ],
             image: const DecorationImage(
                 image: AssetImage(
                   pathImageNews1,
@@ -64,21 +58,64 @@ class SubNews extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: 150,
-      height: 150,
+
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(8),
         color: white,
-        image: DecorationImage(
-            image: AssetImage(
-              image,
+        // image: DecorationImage(
+        //     image: AssetImage(
+        //       image,
+        //     ),
+        //     alignment: Alignment.topCenter),
+        // boxShadow: [
+        //   BoxShadow(
+        //       color: Theme.of(context).colorScheme.shadow.withOpacity(0.10),
+        //       blurRadius: 10,
+        //       spreadRadius: 1,
+        //       offset: const Offset(0, 5)),
+        // ],
+      ),
+      child: IColumn(
+        gap: 5,
+        children: [
+          Image.asset(pathImageNews2),
+          Padding(
+            padding: const EdgeInsets.all(5),
+            child: IColumn(
+              gap: 10,
+              children: [
+                Text(
+                  'Sosialisasi Bersama Tim Pusat Kesejahteraan Sosial (Puskesos) Kabupaten Blora',
+                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                        color: black,
+                        fontWeight: FontWeight.w500,
+                      ),
+                  textAlign: TextAlign.center,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Berita',
+                      style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                            color: black,
+                            fontWeight: FontWeight.w500,
+                          ),
+                      textAlign: TextAlign.center,
+                    ),
+                    Text(
+                      '24 Mar 2024',
+                      style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                            color: black,
+                            fontWeight: FontWeight.w400,
+                          ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ],
             ),
-            alignment: Alignment.topCenter),
-        boxShadow: [
-          BoxShadow(
-              color: Theme.of(context).colorScheme.shadow.withOpacity(0.10),
-              blurRadius: 10,
-              spreadRadius: 1,
-              offset: const Offset(0, 5)),
+          ),
         ],
       ),
       // alignment: Alignment.topCenter,
