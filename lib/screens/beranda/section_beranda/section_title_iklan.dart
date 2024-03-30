baab44c78c333b1cf5568e5b58c83365594ca7bf
marwansaputro <marwansaputro26@gmail.com>
2024-03-30 23:44:51 +0700

    update

--- lib/screens/beranda/section_beranda/section_title_iklan.dart
+++ lib/screens/beranda/section_beranda/section_title_iklan.dart
@@ -10,27 +10,30 @@ class SectionTitleIklan extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Text(
-          "Promotions & Information",
-          style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                color: black,
-                fontWeight: FontWeight.w500,
-              ),
-        ),
-        const Spacer(),
-        TextButton(
-          style: TextButton.styleFrom(
-            foregroundColor: Colors.black54, // foreground
-          ),
-          onPressed: () {},
+        Padding(
+          padding: const EdgeInsets.only(bottom: 10, top: 5),
           child: Text(
-            'See All',
-            style: Theme.of(context).textTheme.titleSmall?.copyWith(
-                  color: primaryGreen,
+            "Promotions & Information",
+            style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                  color: black,
                   fontWeight: FontWeight.w500,
                 ),
           ),
         ),
+        // const Spacer(),
+        // TextButton(
+        //   style: TextButton.styleFrom(
+        //     foregroundColor: Colors.black54, // foreground
+        //   ),
+        //   onPressed: () {},
+        //   child: Text(
+        //     'See All',
+        //     style: Theme.of(context).textTheme.titleSmall?.copyWith(
+        //           color: primaryGreen,
+        //           fontWeight: FontWeight.w500,
+        //         ),
+        //   ),
+        // ),
       ],
     );
   }
