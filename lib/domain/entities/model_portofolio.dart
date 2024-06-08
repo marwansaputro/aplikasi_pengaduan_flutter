1c2881802e94521d17aec11f5ee8be4ac76c232a
wahyu setiawan <dev.wahyu.creator911@gmail.com>
2024-06-09 06:56:48 +0700

    add entities portofolio

--- lib/domain/entities/model_portofolio.dart
+++ lib/domain/entities/model_portofolio.dart
@@ -1,1 +1,14 @@
+class ModelPortofolio {
+  final String image;
+  final String title;
+  final String description;
+  final String location;
+  final String tag;
 
+  ModelPortofolio(
+      {required this.image,
+      required this.title,
+      required this.description,
+      required this.location,
+      required this.tag});
+}
