56506a83404b673d3fd807af3357e194045ce691
wahyu setiawan <dev.wahyu.creator911@gmail.com>
2024-06-11 10:43:38 +0700

    update app value

---
+++ lib/app/config/app_value/app_portofolios.dart
@@ -0,0 +1,101 @@
+import 'package:integra_mobile/app/config/app_constant.dart';
+import 'package:integra_mobile/domain/entities/model_portofolio.dart';
+
+List<ModelPortofolio> appPortofolios = [
+  ModelPortofolio(
+    image: pathImagePortfolioUII,
+    title: 'E-Office Universitas Islam Indonesia',
+    description:
+        '“Aplikasi sistem persuratan elektronik berbasis web & android yang diimplementasikan di Universitas Islam Indonesia”',
+    location: 'Universitas Islam Indonesia,',
+    tag: 'Pendidikan',
+  ),
+  ModelPortofolio(
+    image: pathImagePortfolioBI,
+    title: 'Aplikasi Big Data Inflasi Bank Indonesia DIY',
+    description:
+        '“Aplikasi pengelolaan data & monitoring inflasi di Daerah Istimewa Yogyakarta yang dikembangkan Bank Indonesia”',
+    location: 'KPW Bank Indonesia DIY,',
+    tag: 'Bisnis',
+  ),
+  ModelPortofolio(
+    image: pathImagePortfolioTrenggalek,
+    title: 'Sistem Penanggulangan Kemiskinan Kab. Trenggalek',
+    description:
+        '“Aplikasi Pengelolaan Data dan Program Pengentasan Kemiskinan yang diimplementasikan di Kabupaten Trenggalek”',
+    location: 'Pemkab Trenggalek,',
+    tag: 'Pemerintahan',
+  ),
+  ModelPortofolio(
+    image: pathImagePortfolioDisnaker,
+    title: 'Sistem Aduan Tenaga Kerja Disnakertrans DIY',
+    description:
+        '“Aplikasi Pengelolaan Aduan Tenaga Kerja yang diimplementasikan di Daerah Istimewa Yogyakarta”',
+    location: 'Pemda DIY,',
+    tag: 'Pemerintahan',
+  ),
+  ModelPortofolio(
+    image: pathImagePortfolioPrawirotaman,
+    title: 'Sistem Reservasi Sewa Ruangan UPT Pusat Bisnis',
+    description:
+        '“Aplikasi Manajemen Reservasi sewa ruangan UPT Pusat Bisnis yang diimplementasikan di Pasar Prawirotaman Yogyakarta”',
+    location: 'Bank BPD DIY,',
+    tag: 'Bisnis',
+  ),
+  ModelPortofolio(
+    image: pathImagePortfolioSimtaru,
+    title: 'Sistem Informasi Tata Ruang Kabupaten Sleman',
+    description:
+        '“Sistem Informasi manajemen yang digunakan untuk pendataan rencana detail tata ruang di Kabupaten Sleman”',
+    location: 'Pemkab Sleman,',
+    tag: 'Pemerintahan',
+  ),
+  ModelPortofolio(
+    image: pathImagePortfolioDestinasi,
+    title: 'Aplikasi Tiketing dan Manajemen Destinasi Wisata',
+    description:
+        '“Aplikasi pengelolaan tiketing destinasi wisata dan sistem manajemen pengelolaan objek wisata”',
+    location: 'Destinasi Wisata Indonesia,',
+    tag: 'Bisnis',
+  ),
+  ModelPortofolio(
+    image: pathImagePortfolioAlkes,
+    title: 'Sistem Pengawasan Alat Kesehatan dan PKRT',
+    description:
+        '“Aplikasi E-Watch & E-Pengawasan – Sistem Pengawasan Alat Kesehatan dan PKRT Kementerian Kesehatan Republik Indonesia”',
+    location: 'Kementerian Kesehatan,',
+    tag: 'Pemerintahan',
+  ),
+  ModelPortofolio(
+    image: pathImagePortfolioVisiting,
+    title: 'Aplikasi Pendataan Wisatawan Visiting Jogja',
+    description:
+        '“Aplikasi Pengelolaan Data Kunjungan Wisatawan dan Reservasi Tiket Destinasi Wisata di DIY”',
+    location: 'Pemda DIY,',
+    tag: 'Pemerintahan',
+  ),
+  ModelPortofolio(
+    image: pathImagePortfolioSPPIRT,
+    title: 'Aplikasi Digitalisasi Layanan SP-PIRT Kota Yogyakarta',
+    description:
+        '““Aplikasi Digitalisasi Layanan Sertifikasi Produk Pangan Industri Rumah Tangga Kota Yogyakarta”',
+    location: 'Pemkot Yogyakarta,',
+    tag: 'Pemerintahan',
+  ),
+  ModelPortofolio(
+    image: pathImagePortfolioCovid,
+    title: 'Aplikasi Covid Tracer Kabupaten Sleman',
+    description:
+        '“Aplikasi Pengelolaan Data Covid19 dan Tracing Sistem di Kabupaten Sleman”',
+    location: 'Pemkab Sleman,',
+    tag: 'Pemerintahan',
+  ),
+  ModelPortofolio(
+    image: pathImagePortfolioCovid,
+    title: 'Aplikasi E-Health Kabupaten Blitar',
+    description:
+        '“Aplikasi informasi dan Layanan publik kesehatan di Kabupaten Blitar berbasis web & mobile”',
+    location: 'Pemkab Blitar,',
+    tag: 'Pemerintahan',
+  ),
+];
