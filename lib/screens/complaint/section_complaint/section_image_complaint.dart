decbe52e6d126dfae3bc2014c9e2d4c4592b28b2
wahyu setiawan <dev.wahyu.creator911@gmail.com>
2024-06-12 21:41:45 +0700

    refacoring

--- lib/screens/complaint/section_complaint/section_image_complaint.dart
+++ lib/screens/complaint/section_complaint/section_image_complaint.dart
@@ -2,11 +2,8 @@ import 'dart:io';
 
 import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:integra_mobile/layout/padding.dart';
-import 'package:integra_mobile/layout/row.dart';
 import 'package:integra_mobile/app/config/app_constant.dart';
-import 'package:integra_mobile/app/config/theme.dart';
+import 'package:integra_mobile/share/widget/mocullar/bottom_sheet/bottom_sheet_change_image.dart';
 
 class SectionImageComplaint extends StatefulWidget {
   const SectionImageComplaint({
@@ -26,144 +23,24 @@ class _SectionImageComplaintState extends State<SectionImageComplaint> {
 
   set file(File? file) {
     _file = file;
+    isFile = file != null;
 
     widget.changeImage?.call(_file);
-  }
-
-  void takeImageWithCamera() async {
-    XFile? xFileImage =
-        await ImagePicker().pickImage(source: ImageSource.camera);
-    File image = File(xFileImage!.path);
-    Navigator.of(context).pop();
-
-    setState(() {
-      file = image;
-      isFile = true;
-    });
-  }
-
-  void takeImageWithGallery() async {
-    XFile? xFileImage =
-        await ImagePicker().pickImage(source: ImageSource.gallery);
-    File image = File(xFileImage!.path);
-    Navigator.of(context).pop();
-
-    setState(() {
-      file = image;
-      isFile = true;
-    });
-  }
-
-  void cencel() {
-    Navigator.of(context).pop();
   }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        showModalBottomSheet(
-          shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(30), topRight: Radius.circular(30))),
+        showBottomSheet(
           context: context,
-          builder: (BuildContext context) {
-            return SizedBox(
-              height: 150,
-              child: Padding(
-                padding: paddingMobile,
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 20),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Container(
-                        height: 4,
-                        width: 40,
-                        color: darkGrey,
-                      ),
-                      const SizedBox(height: 10),
-                      Text(
-                        'Select your complaint file',
-                        style:
-                            Theme.of(context).textTheme.headlineSmall?.copyWith(
-                                  color: black,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                      ),
-                      const SizedBox(height: 20),
-                      IRow(
-                        gap: 10,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Expanded(
-                            child: ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: primaryGreen,
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(8),
-                                ),
-                              ),
-                              onPressed: takeImageWithCamera,
-                              child: IRow(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                gap: 5,
-                                children: [
-                                  const Icon(
-                                    Icons.camera,
-                                    color: white,
-                                  ),
-                                  Text(
-                                    'Camera',
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .titleLarge
-                                        ?.copyWith(
-                                          color: white,
-                                          fontWeight: FontWeight.w500,
-                                        ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                          Expanded(
-                            child: ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: primaryGreen,
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(8),
-                                ),
-                              ),
-                              onPressed: takeImageWithGallery,
-                              child: IRow(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                gap: 5,
-                                children: [
-                                  const Icon(
-                                    Icons.photo,
-                                    color: white,
-                                  ),
-                                  Text(
-                                    'Gallery',
-                                    style: Theme.of(context)
-                                        .textTheme
-                                        .titleLarge
-                                        ?.copyWith(
-                                          color: white,
-                                          fontWeight: FontWeight.w500,
-                                        ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+          builder: (context) {
+            return BottomSheetChangeImage(
+              onImagePick: (image) {
+                file = image;
+
+                Navigator.of(context).pop();
+              },
             );
           },
         );
