8bb36c7ebfd66b8f74d43b878526b60cf84255f4
wahyu setiawan <dev.wahyu.creator911@gmail.com>
2024-06-13 20:53:57 +0700

    menambahakn api change password

--- lib/data/provider/network/api/api_user.dart
+++ lib/data/provider/network/api/api_user.dart
@@ -49,3 +49,20 @@ Future<ReturnMessage> apiUpdateChangeImageProfile({required File image}) async {
 
   throw Exception('Tidak berhasil merubah image user');
 }
+
+Future<ReturnMessage> apiUpdatePasssword(
+    {required String oldPassword, required String newPassword}) async {
+  final data = await myDio().put(
+    '/api/user-change-password',
+    data: {
+      'oldPassword': oldPassword,
+      'newPassword': newPassword,
+    },
+  );
+
+  if (data.statusCode == 201) {
+    return ReturnMessage.fromJson(jsonDecode(data.toString()));
+  }
+
+  throw Exception('Tidak berhasil merubah image user');
+}
