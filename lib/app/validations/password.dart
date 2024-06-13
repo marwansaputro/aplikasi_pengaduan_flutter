30eb0308fec609ebacd9754c22effd93f14d10b7
wahyu setiawan <dev.wahyu.creator911@gmail.com>
2024-06-13 09:52:26 +0700

    add validation form into bottom sheet sign in sign out dan forgot password

--- lib/app/validations/password.dart
+++ lib/app/validations/password.dart
@@ -2,6 +2,15 @@ import 'package:formz/formz.dart';
 
 enum PasswordValidationError { empty }
 
+extension ExtPasswordvalidationError on PasswordValidationError {
+  String? get text {
+    switch (this) {
+      case PasswordValidationError.empty:
+        return "Password tidak boleh kosong";
+    }
+  }
+}
+
 class PasswordRegistration extends FormzInput<String, PasswordValidationError> {
   const PasswordRegistration.pure() : super.pure("");
   const PasswordRegistration.dirty([super.value = '']) : super.dirty();
